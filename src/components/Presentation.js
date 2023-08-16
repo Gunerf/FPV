0cd6795a73c438b34bf9deacfcc94c5a83ea1965
Gunerf <schwerdtlenicolas@gmail.com>
2023-08-16 14:58:10 +0200

    presentation page

---
+++ src/components/Presentation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Presentation.css'
+
+function Presentation() {
+  return (
+    <div className='presentation'>
+            <img className='pp' alt='un portrait de Slohan Pinaud' src='moi.jpg'></img>
+        <div className='texte-container'>
+            <h1>Slohan PINAUD</h1>
+            <p>Tam quam vere vel vere conscriptis abstulit sociis tam est deos multis genere vel P vel vere cum est est ad quos fortuna moriendi vel domum vel vel celeritas quos domum potius nihil quam sensum genere vel P est pervenisse homines ex homines vel ut superos deos patribus inferos celeberrimos. Tam quam vere vel vere conscriptis abstulit sociis tam est deos multis genere vel P vel vere cum est est ad quos fortuna moriendi vel domum vel vel celeritas quos domum potius nihil quam sensum genere vel P est pervenisse homines ex homines vel ut superos deos patribus inferos celeberrimos.Tam quam vere vel vere conscriptis abstulit sociis tam est deos multis genere vel P vel vere cum est est ad quos fortuna moriendi vel domum vel vel celeritas quos domum potius nihil quam sensum genere vel P est pervenisse homines ex homines vel ut superos deos patribus inferos celeberrimos.Tam quam vere vel vere conscriptis abstulit sociis tam est deos multis genere vel P vel vere cum est est ad quos fortuna moriendi vel domum vel vel celeritas quos domum potius nihil quam sensum genere vel P est pervenisse homines ex homines vel ut superos deos patribus inferos celeberrimos.Tam quam vere vel vere conscriptis abstulit sociis tam est deos multis genere vel P vel vere cum est est ad quos fortuna moriendi vel domum vel vel celeritas quos domum potius nihil quam sensum genere vel P est pervenisse homines ex homines vel ut superos deos patribus inferos celeberrimos.Tam quam vere vel vere conscriptis abstulit sociis tam est deos multis genere vel P vel vere cum est est ad quos fortuna moriendi vel domum vel vel celeritas quos domum potius nihil quam sensum genere vel P est pervenisse homines ex homines vel ut superos deos patribus inferos celeberrimos.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Presentation
--- src/pages/App.js
+++ src/pages/App.js
@@ -2,12 +2,14 @@ import '../styles/App.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Navigation from '../components/Navigation';
+import Presentation from '../components/Presentation';
 
 function App() {
   return (
     <>
     <Header />
     <Navigation />
+    <Presentation />
     <Footer />
     </>
   );
